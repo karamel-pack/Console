2eb7167f2a2846d6473c3a212fa3818d927f6b4f
amir-kalantar <amir.kalantar74@gmail.com>
2020-11-01 14:28:15 +0330

    add define for controller namespace

--- src/Commands/Make/Controller.php
+++ src/Commands/Make/Controller.php
@@ -16,14 +16,18 @@ class ###CLASSNAME### {
 }";
 
         global $argv;
-        $explode = explode('/', $argv[2]);
-        $filename = array_pop($explode);
+        $file_path = $argv[2];
+        if (defined("CONTROLLER_NAMESPACE"))
+            $file_path = CONTROLLER_NAMESPACE . "/" . $file_path;
 
+        $explode = explode('/', $file_path);
+
+        $filename = array_pop($explode);
 
         if (!is_dir(implode("/", $explode)))
             mkdir(implode("/", $explode), 0777, true);
 
-        $file = fopen($argv[2] . '.php', 'w+');
+        $file = fopen($file_path . '.php', 'w+');
         $temp = str_replace('###NAMESPACE###', implode('\\', $explode), $temp);
         $temp = str_replace('###CLASSNAME###', $filename, $temp);
         fwrite($file, $temp);
