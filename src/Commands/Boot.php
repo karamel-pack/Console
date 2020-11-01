575f01a48dc735e3224a18b4e8b3a430a030fd4d
amir-kalantar <amir.kalantar74@gmail.com>
2020-11-01 15:00:42 +0330

    create Serve Command

--- src/Commands/Boot.php
+++ src/Commands/Boot.php
@@ -8,6 +8,7 @@ class Boot
     {
         $console->add("make:model","Create Model" , '\Karamel\Console\Commands\Make\Model::define');
         $console->add("make:controller","Create Controller" , '\Karamel\Console\Commands\Make\Controller::define');
+        $console->add("serve","runnig server " , '\Karamel\Console\Commands\Serve::define');
 //        Console::add("make:model","Create Model" , '\Karamel\Commands\Make\Model::define');
 
     }
---
+++ src/Commands/Serve.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Karamel\Console\Commands;
+
+use Karamel\Console\Interfaces\ICommand;
+
+class Serve implements ICommand
+{
+    public static function define()
+    {
+        global $argv;
+        $port = 8080;
+        $public = "public/";
+        foreach ($argv as $key => $item) {
+            $tmp = explode("=", $item);
+            if (count($tmp) === 2) {
+                if ($tmp[0] === "--port")
+                    $port = $tmp[1];
+
+            }
+        }
+        foreach ($argv as $key => $item) {
+            $tmp = explode("=", $item);
+            if (count($tmp) === 2) {
+                if ($tmp[0] === "--t")
+                    $public = $tmp[1];
+
+            }
+        }
+
+        echo "Server has been start at :" . $port . " And Document root is " . $public . "\n";
+        exec('php -S localhost:' . $port . ' -t ' . $public);
+    }
+}
