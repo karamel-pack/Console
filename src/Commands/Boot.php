1dca7400ec6473eedad352c48a9868eb31a36784
amir-kalantar <amir.kalantar74@gmail.com>
2020-11-01 14:20:07 +0330

    add create Controller command to boot file

--- src/Commands/Boot.php
+++ src/Commands/Boot.php
@@ -7,6 +7,7 @@ class Boot
     public static function start(Console $console)
     {
         $console->add("make:model","Create Model" , '\Karamel\Console\Commands\Make\Model::define');
+        $console->add("make:controller","Create Controller" , '\Karamel\Console\Commands\Make\Controller::define');
 //        Console::add("make:model","Create Model" , '\Karamel\Commands\Make\Model::define');
 
     }
---
+++ src/Commands/Make/Controller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Karamel\Console\Commands\Make;
+use Karamel\Console\Interfaces\ICommand;
+
+class Controller implements ICommand
+{
+
+    public static function define()
+    {
+        // TODO: Implement define() method.
+    }
+}
